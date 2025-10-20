d39be5624f38a9b08b06432b864bd559c6c66510
Denislav883 <yanev.denislav@gmail.com>
2025-10-20 17:01:44 +0300

    Add mongoose

--- api/src/index.js
+++ api/src/index.js
@@ -1,9 +1,22 @@
 import express from "express";
 import cors from "cors";
+import mongoose from "mongoose";
 
 import routes from "./routes.js";
 
 const app = express();
+
+// Setup mongoose
+    try {
+        await mongoose.connect("mongodb://localhost:27017", {
+            dbName: "furniture"
+    });
+
+    console.log("Successfully connected to DB!");
+} catch(err) {
+    console.error("Cannot connect to db!");
+    console.error(err.nessage);
+}
 
 // Add cors
 app.use(cors());
